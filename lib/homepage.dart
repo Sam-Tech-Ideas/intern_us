d3e97fb26d4905138c458949c2923274a8d57b7d
Sam-Tech-Ideas <agyemangsamuel235@gmail.com>
2022-09-10 11:59:29 +0000

    pages update

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-
-
+import './screens/home/home.dart';
+import './screens/search/search.dart';
+import './screens/profile/profile.dart';
 class HomePage extends StatefulWidget {
   const HomePage({ Key? key }) : super(key: key);
 
@@ -9,10 +10,42 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+
+  void _navBottom(int index){
+    setState(() {
+      _selectedIndex = index;
+      
+    });
+  }
+
+  final List<Widget>_pages =[
+    Home(),
+    Search(),
+    Profile(),
+
+  ] ;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ,
+      body:Center(
+           child: Text(
+            "Home",style: TextStyle(
+              fontSize: 50
+            ),
+           ),
+ ) ,
+ bottomNavigationBar: BottomNavigationBar(
+  onTap: _navBottom,
+  currentIndex: _selectedIndex,
+  
+  items: [
+  BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+  BottomNavigationBarItem(icon: Icon(Icons.search), label: "Search"),
+  BottomNavigationBarItem(icon: Icon(Icons.person),label: "Profile"),
+  //BottomNavigationBarItem(icon: Icon(Icons.home)),
+  
+ ]),
     );
   }
 }
