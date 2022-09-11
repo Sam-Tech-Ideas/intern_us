ed3236cc9161b4196bed2b6751ea644790679f2f
Sam-Tech-Ideas <agyemangsamuel235@gmail.com>
2022-09-11 23:03:56 +0000

    update

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,10 @@ import 'package:flutter/material.dart';
 
 import 'package:intern_us/screens/splash/splash.dart';
 import 'homepage.dart';
-//import 'package:firebase_core/firebase_core.dart';
-void main()  {
- // WidgetsFlutterBinding.ensureInitialized();
-//  await Firebase.initializeApp();
+import 'package:firebase_core/firebase_core.dart';
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
